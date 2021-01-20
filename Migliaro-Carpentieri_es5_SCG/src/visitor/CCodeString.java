e3c67db50d4c136c667195513aeef8a23a8296cd
loren <l.carpentieri7@studenti.unisa.it>
2021-01-20 18:23:55 +0100

    Solved some problem with inputString

--- Migliaro-Carpentieri_es5_SCG/src/visitor/CCodeString.java
+++ Migliaro-Carpentieri_es5_SCG/src/visitor/CCodeString.java
@@ -3,16 +3,18 @@ package visitor;
 public final class CCodeString
 {
 	public static final String deleteSubstring = "char* deleteSubstring(char* str, char* sub)\n" +
-												 "{\n" +
+									 			 "{\n" +
 												 "    char *tempStr = str;\n" +
 												 "    char *result = malloc(sizeof(char) * (strlen(str) + 1));\n" +
-												 "\n" +
+												 "    result[0] = '\\0';\n" +
+												 "    \n" +
 												 "    while(tempStr != '\\0')\n" +
 												 "    {\n" +
 												 "        if(strncmp(tempStr, sub, strlen(sub)))\n" +
 												 "        {\n" +
-												 "            char *dest = malloc(sizeof(char));\n" +
+												 "            char *dest = malloc(sizeof(char) * 2);\n" +
 												 "            *dest = *tempStr;\n" +
+												 "            dest[1] = '\\0';\n" +
 												 "            result = strcat(result, dest);\n" +
 												 "            tempStr++;\n" +
 												 "        }\n" +
@@ -24,9 +26,9 @@ public final class CCodeString
 												 "        }\n" +
 												 "\n" +
 												 "    }\n" +
-												 "	  \n" +
+												 "\t  \n" +
 												 "    return result;\n" +
-												 "}";
+												 "}\n";
 
 	public static final String countOccurrences = "int countOccurrences(char* string, char* substring)\n" +
 												  "{\n" +
@@ -40,33 +42,36 @@ public final class CCodeString
 												  "            tmp += substringLen;\n" +
 												  "            count++;\n" +
 												  "        }\n" +
-			 									  "    \n" +
+												  "    \n" +
 												  "    return count;\n" +
-												  "}";
+												  "}\n";
 
 	public static final String concatString = "char* concatString(char *str1, char *str2)\n" +
-					                          "{\n" +
+			                                  "{\n" +
 											  "    char *result = malloc(sizeof(char) * (strlen(str1) + strlen(str2) + 1));\n" +
-			                                  "    strcat(result, str1);\n" +
-			                                  "    strcat(result, str2);\n" +
-			                                  "    \n" +
-			                                  "    return result;\n" +
-			                                  "}";
+											  "    result[0] = '\\0';\n" +
+											  "    strcat(result, str1);\n" +
+											  "    strcat(result, str2);\n" +
+											  "    \n" +
+											  "    return result;\n" +
+											  "}\n";
 
 	public static final String repeatString = "char* repeatString(char *str, int n)\n" +
-											  "{\n" +
-									          "    char *result = malloc(sizeof(char) * (strlen(str) * n + 1));\n" +
+			                                  "{\n" +
+											  "    char *result = malloc(sizeof(char) * (strlen(str) * n + 1));\n" +
+											  "    result[0] = '\\0';\n" +
 			                                  "    for (int i = 0; i < n; i++)\n" +
-			                                  "        result = concatString(result, str);\n" +
-			                                  "    \n" +
-			                                  "    return result;\n" +
-			                                  "}";
+											  "        result = concatString(result, str);\n" +
+											  "    \n" +
+											  "    return result;\n" +
+											  "}\n";
 
 	public static final String inputString = "char* inputString()\n" +
 											 "{\n" +
 											 "    char *result = malloc(sizeof(char));\n" +
-											 "    char *ch = malloc(sizeof(char));\n" +
-											 "    \n" +
+											 "    char *ch = malloc(sizeof(char)*2);\n" +
+											 "    result[0] = '\\0';\n" +
+											 "    ch[1] = '\\0';\n" +
 											 "    while((*ch = getchar()) != '\\n')\n" +
 											 "        result = concatString(result, ch);\n" +
 											 "    \n" +
